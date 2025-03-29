0108d5936997422a9155a85684ca2164cf636ea6
Sasguez <marioard5644@hotmail.com>
2025-03-29 16:54:08 -0500

    principios ejercicio 10

---
+++ ejercicio10/js/ejercicio10.js
@@ -0,0 +1,10 @@
+let numTest = 10
+
+for(let n = 0; n < numTest; n++){
+//   console.log('el numero: ', n);
+console.log('test residuo numero:: ', n %3);
+
+  if(n % 2 && n / (n-1)){
+      console.log('el numero: ', n);
+  }
+}
